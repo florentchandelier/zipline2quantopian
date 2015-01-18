1c520afdec4f3b358b5157f97e75070f4dd37583
Florent Chandelier <florent.chandelier@elucidatedip.com>
2015-01-18 09:27:49 -0500

    complement generic modules with info to be added in main algo

--- skeleton/generic_modules/perf_analysis.py
+++ skeleton/generic_modules/perf_analysis.py
@@ -1,6 +1,12 @@
 from necessary_import import *
 
 def get_cagr(context, data):
+''' in [def initialize(context)] add 
+	- context.cagr_period = 0
+	- in Z & Q: context.schedule_function(get_cagr,
+                      date_rule=date_rules.month_start(days_offset=5),
+                      time_rule=time_rules.market_open(hours=1, minutes=0))
+'''
     context.cagr_period += 1
     if (context.cagr_period % 12 == 0):
         # portf_value: Sum value of all open positions and ending cash balance. 
