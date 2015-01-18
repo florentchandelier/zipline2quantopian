831a8eb3a5f4164383966a5dc66c87152bdb5e0d
Florent Chandelier <florent.chandelier@elucidatedip.com>
2015-01-18 09:43:47 -0500

    updating example and script to work with symlinks

---
+++ example/paired_switching_strategy/p_switching_zipline.py
@@ -0,0 +1,28 @@
+
+from global_import.zipline_import import *
+from p_switching.psw_core import *
+
+
+if __name__ == '__main__':
+ 
+    algo = TradingAlgorithm(initialize=initialize, handle_data=handle_data, capital_base = 10000)
+    
+    #
+    # Fetch and Load financial Instruments
+    #
+    # This process is required if you declare more than one list of instruments in your algorithm.
+    # This is often the case if a strategy applies filters to a list of stocks, and
+    # default to cash (or any other instrument) should none of the stocks satisfy
+    # the criteria, or a cash-stop situation is reached.
+    #
+    new_data = [''.join(w) for w in algo.stocks]
+    #new_data += [algo.cashETF]
+    data = load_from_yahoo(stocks=new_data, indexes={},start=algo.startDate, end=algo.endDate)
+    data = data.dropna()
+    #
+    # End Of Fetch and Load
+    #
+    
+    results = algo.run(data)
+    plot_portfolio(results)
+
