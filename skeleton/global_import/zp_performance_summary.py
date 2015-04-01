1fa9f22f315ce618f8d379e5014a4d7530cd6584
Florent Chandelier <florent.chandelier@elucidatedip.com>
2015-04-01 10:27:03 -0400

    addin gain to pain ratio and + using render function to render metrics

--- skeleton/global_import/zp_performance_summary.py
+++ skeleton/global_import/zp_performance_summary.py
@@ -64,39 +64,76 @@ class performance():
          or the first_n biggest drawdowns;
          # http://stackoverflow.com/questions/27060037/pandas-way-to-groupby-like-itertools-groupby
         '''
-        
+            
         self.make_df()
         self.make_dd()
         
         x = -100* (self.df['drawdowns']/self.df['highwatermark'])
         x[np.isnan(x)]=0
     
+        if abs(thresh_dd) == 0:
+            thresh_dd = 0.000001
         if thresh_dd >0: thresh_dd = -thresh_dd
     
         # this should return the max depth and length
         mask = x <= thresh_dd
         groupnum = mask.diff().fillna(method='bfill').cumsum()
-            
-        print("\n DRAWDOWNS ANALYSIS \n ------------------")
-        print(" DD period below the " +str(thresh_dd) +"% watermark" )
-        
+   
         if max(groupnum) == 0:
-            print("   -> No occurence of DD <= " +str(thresh_dd) +"%")
-            print(" Max DD is " +str(round(min(x),2)) +"%" )
-            return None
+#            print("   -> No occurence of DD <= " +str(thresh_dd) +"%")
+#            print(" Max DD is " +str(round(min(x),2)) +"%" )
+            return []
             
-        col = ['From', 'maxDD', 'To', 'mDD', 'Length', 'To mDD', 'Recovery']
+        col = ['From', 'maxDD', 'To', 'mDD-%', 'mDD-$', 'Length', 'To mDD', 'Recovery']
         temp_df = pd.DataFrame(columns=col)
         
         for key,grp in x.groupby(groupnum):
             if (max(grp) <= thresh_dd):
                 length = len(grp)-1
                 temp_df.loc[len(temp_df)] = [ grp.index[0].strftime("%Y-%m-%d"), grp.idxmin().strftime("%Y-%m-%d") ,grp.index[length].strftime("%Y-%m-%d"), 
-                       grp.min(), (grp.index[length]-grp.index[0]).days, (grp.argmin()-grp.index[0]).days, (grp.index[length]-grp.argmin()).days]
+                       grp.min(), abs(round(grp.min()*self.df['highwatermark'][grp.idxmin()]/100)), (grp.index[length]-grp.index[0]).days, (grp.argmin()-grp.index[0]).days, (grp.index[length]-grp.argmin()).days]
         
-        tot_days = (self.df['portfolio_value'].index[len(self.df['portfolio_value'])-1] - self.df['portfolio_value'].index[0]).days
-        print(" %-time in High DD watermark: " +str(round(100*temp_df['Length'].sum()/tot_days ,0) ) +"% \n") 
-        print(" Max DD is " +str(round(min(temp_df['mDD']),2)) +"%" )
-        print temp_df
+#        tot_days = (self.df['portfolio_value'].index[len(self.df['portfolio_value'])-1] - self.df['portfolio_value'].index[0]).days
+#        print(" %-time in High DD watermark: " +str(round(100*temp_df['Length'].sum()/tot_days ,0) ) +"% \n") 
+#        print(" Max DD is " +str(round(min(temp_df['mDD-%']),2)) +"%" )
+#        print temp_df
         
         return temp_df
+        
+    def render_from_trough_to_depth_trough(self, thresh_dd=0):
+        dd = self.get_from_trough_to_depth_trough(thresh_dd, first_n=0)
+        
+        if len(dd) == 0:
+            print("   -> No occurence of DD <= " +str(thresh_dd) +"%")
+            x = -100* (self.df['drawdowns']/self.df['highwatermark'])
+            x[np.isnan(x)]=0
+            print(" Max DD is " +str(round(min(x),2)) +"%" )
+            return
+            
+        print("\n DRAWDOWNS ANALYSIS \n ------------------")
+        print(" DD period below the " +str(thresh_dd) +"% watermark" )
+        
+        tot_days = (self.df['portfolio_value'].index[len(self.df['portfolio_value'])-1] - self.df['portfolio_value'].index[0]).days
+        print(" %-time in High DD watermark: " +str(round(100*dd['Length'].sum()/tot_days ,0) ) +"% \n") 
+        print(" Max DD is " +str(round(min(dd['mDD-%']),2)) +"%" )
+        print dd
+        
+        return
+        
+    def get_gain_to_pain (self):
+        '''
+        ref: Building reliable trading systems
+        gain-to-pain metric is a ratio formed by dividing the average annual return in $ (the gain) by the average annual max drawdown in $ (the pain you experience to get your gain)
+        -> the average annual maxDD is the average of the x largest DD divided by the number of year in the backtest, where x is the number of years        
+        '''
+        dd = self.get_from_trough_to_depth_trough(thresh_dd=0)
+        
+        nb_year = (self.df['portfolio_value'].idxmax().year - self.df['portfolio_value'].idxmin().year) +1
+        gain = self.df['portfolio_value'][-1] - self.df['portfolio_value'][0]
+        avg_dd = np.mean(dd.sort(['mDD-$'], ascending=False)[0:nb_year]['mDD-$'])
+        
+        return gain/ (avg_dd*nb_year)
+        
+    def render_get_gain_to_pain (self):
+        g2p = self.get_gain_to_pain()
+        print("The gain-to-pain ration is " +str(g2p))
